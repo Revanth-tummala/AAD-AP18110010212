36791118f069e0df554742ed497d7a2c0df16ee0
revanth <tummalarevanth95@gmail.com>
2020-07-13 00:00:45 +0530

    task added

---
+++ task7_contextmenu/app/src/main/java/com/example/task7_contextmenu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.task7_contextmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b=findViewById(R.id.button);
+        registerForContextMenu(b);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.list_menu,menu);
+
+    }
+
+
+}
